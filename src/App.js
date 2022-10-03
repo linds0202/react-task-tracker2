4d5dc236a1a6875c61096279c23f082d4edb7962
linds0202 <lindsayarainey@gmail.com>
2022-10-03 12:31:27 -0700

    added backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
@@ -23,25 +23,86 @@ function App() {
     }
 ])
 
+useEffect(() => {
+  const getTasks = async () => {
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+  }
+  
+  getTasks()
+}, [])
+
+//Fetch tasks
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = res.json()
+
+  return data
+}
+
+//Fetch a single task
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = res.json()
+
+  return data
+}
+
+
 
 //Add task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-tpye': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
 
-  const newTask = {id, ...task}
+  const data = await res.json()
 
-  setTasks([...tasks, newTask])
+  setTasks([...tasks, data])
+
+
+  // const id = Math.floor(Math.random() * 10000) + 1
+
+  // const newTask = {id, ...task}
+
+  // setTasks([...tasks, newTask])
 }
 
 //Delete task
 
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE'
+  })
   setTasks(tasks.filter(task => task.id !== id))
 }
 
 //Toggle reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+
+  const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-tpye': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
+  setTasks(
+    tasks.map(task => 
+      task.id === id ? 
+      { ...task, reminder: data.reminder } : 
+      task
+    ))
 }
 
 return (
